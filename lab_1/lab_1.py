bad5266526c8e90c873a551b09f980bb08906f9d
kutniu <kutnijdima@gmail.com>
2023-11-08 11:52:40 +0200

    update lab 1

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -1,38 +1,50 @@
-students = [
-    {"name": "vlad", "phone": "543785412", "email": "bad@email", "address": "tyrtyuihjgfg4"},
-    {"name": "djenya", "phone": "54302154", "email": "good@email", "address": "45uiyuo"},
-    {"name": "elif", "phone": "865431235", "email": "wow@email", "address": "3w31qqawsa"},
-    {"name": "vanya", "phone": "453452244", "email": "some@email", "address": "dgwefsdf"}
+list = [
+    {"name": "Bob", "phone": "543785412", "email": "bad@email", "address": "tyrtyuihjgfg4"},
+    {"name": "Emma", "phone": "54302154", "email": "good@email", "address": "45uiyuo"},
+    {"name": "Jon", "phone": "865431235", "email": "wow@email", "address": "3w31qqawsa"},
+    {"name": "Zak", "phone": "453452244", "email": "some@email", "address": "dgwefsdf"}
 ]
 
 def printAllList():
-    sorted_students = sorted(students, key=lambda x: x["name"])  
+    sorted_students = sorted(list, key=lambda x: x["name"])  
     for student in sorted_students:
         strForPrint = f"Student name is {student['name']}, Phone is {student['phone']}, Email is {student['email']}, Address is {student['address']}"
         print(strForPrint)
 
 def addNewElement():
-    name = input("Please enter student name: ")
+    name = input("Pease enter student name: ")
     phone = input("Please enter student phone: ")
     email = input("Please enter student email: ")
     address = input("Please enter student address: ")
-    new_student = {"name": name, "phone": phone, "email": email, "address": address}
-    students.append(new_student)
-    students.sort(key=lambda x: x['name']) 
-    print("New student has been added")
+    newItem = {"name": name, "phone": phone, "email": email, "address": address}
+    # find insert position
+    insertPosition = 0
+    for item in list:
+        if name > item["name"]:
+            insertPosition += 1
+        else:
+            break
+    list.insert(insertPosition, newItem)
+    print("New element has been added")
+    return
 
 def deleteElement():
     name = input("Please enter name to be deleted: ")
-    for student in students:
+    deletePosition = -1
+    for student in list:
         if name == student["name"]:
-            students.remove(student)
-            print(f"{name} has been deleted")
-            return
-    print("Student not found")
+            deletePosition = list.index(student)
+            break
+    if deletePosition == -1:
+        print("Element was not found")
+    else:
+        del list[deletePosition]
+        print("Element has been deleted")
+
 
 def updateElement():
     name = input("Please enter name to be updated: ")
-    for index, student in enumerate(students):
+    for index, student in enumerate(list):
         if name == student["name"]:
             new_name = input("Enter new name: ")
             new_phone = input("Enter new phone: ")
@@ -40,14 +52,14 @@ def updateElement():
             new_address = input("Enter new address: ")
             newElement = {"name": new_name, "phone": new_phone, "email": new_email, "address": new_address}
 
-            del students[index]
+            del list[index]
             insertPos = 0
-            for pos, elem in enumerate(students):
+            for pos, elem in enumerate(list):
                 if new_name > elem["name"]:
                     insertPos = pos + 1
                 else:
                     break
-            students.insert(insertPos, newElement)
+            list.insert(insertPos, newElement)
             print("Element has been updated")
             break
     else:
